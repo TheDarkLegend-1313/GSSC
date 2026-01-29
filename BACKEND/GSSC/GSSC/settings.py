ea96a4424d738ace34aff5dc607e2446a25d863e
TheDarkLegend-1313 <muhammadshaheryarirfan@gmail.com>
2026-01-29 19:49:10 +0500

    Update settings.py

--- BACKEND/GSSC/GSSC/settings.py
+++ BACKEND/GSSC/GSSC/settings.py
@@ -161,7 +161,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "thedarklegend6699@gmail.com"
-EMAIL_HOST_PASSWORD = "aygvzjtbkkoblbuq"
+# EMAIL_HOST_PASSWORD = ""
 
 DEFAULT_FROM_EMAIL = "GSSC <thedarklegend6699@gmail.com>"
 
